20824e6f4e07817d25eda0e32ffba271a301dd8d
jdboyd <jdboyd@69126abb-559d-42f5-9e9b-f0010dbe68b2>
2009-02-18 19:06:14 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://home.jdboyd.net/export/home/jdboyd/cvs2svn/svn/trunk/projects/podtrack@241 69126abb-559d-42f5-9e9b-f0010dbe68b2

--- podtrack.py
+++ podtrack.py
@@ -4,18 +4,25 @@ import sys, traceback
 import feedparser
 import datetime
 
-#on ubuntu 8.10 use sqlite3 from CLI instead of just sqlite
-#create table podCast ( pName char(50), pUrl  char(2048), pId INTEGER PRIMARY KEY AUTOINCREMENT);
-#pUrl is the feed item url, while pHref is the enclosure URL
-#create table podItems (pTitle char(1024), pUrl char(2048), pHref char(2048), pId integer, itemId integer primary key autoincrement);
-
 try:
     from sqlite3 import dbapi2 as sqlite
 except:
     from pysqlite2 import dbapi2 as sqlite
 
+#on ubuntu 8.10 use sqlite3 from CLI instead of just sqlite
+#create table podCast ( pName char(50), pUrl  char(2048), pId INTEGER PRIMARY KEY AUTOINCREMENT);
+#pUrl is the feed item url, while pHref is the enclosure URL
+#create table podItems (pTitle char(1024), pUrl char(2048), pHref char(2048), pId integer, itemId integer primary key autoincrement);
+
+def createDb():
+    c = con.cursor()
+    c.execute("create table podCast ( pName char(50), pUrl  char(2048), pId INTEGER PRIMARY KEY AUTOINCREMENT);")
+    c.execute("create table podItems (pTitle char(1024), pUrl char(2048), pHref char(2048), pId integer, itemId integer primary key autoincrement);")
+    con.commit()
+
+con=None
+
 def isNewEntry(e):
-#BUG: Stubbed for later
     c = con.cursor()
     if 'title' in e:
         title = e['title'].encode("utf-8")
@@ -51,9 +58,9 @@ def processEntries(entries, pid):
                 filesToDl.append(enc)
                 t={"t": title, "u": str(link), "h": str(enc), "i": pid}
                 #print repr(t)
-                #c = con.cursor()
-                #c.execute("insert into podItems(pTitle, pUrl, pHref, pId) values(:t,:u,:h,:i);", t)
-                #con.commit()
+                c = con.cursor()
+                c.execute("insert into podItems(pTitle, pUrl, pHref, pId) values(:t,:u,:h,:i);", t)
+                con.commit()
             except KeyError, ke:
                 print "ERROR----------------------------------"
                 print "KEY ERROR:", ke.args[0]
@@ -70,12 +77,9 @@ def grabFeed(l):
 def makeGrabber(l):
     return lambda: grabFeed(l)
 
-if __name__ == "__main__":
-    wq = WorkerPoolSerial(10)
-    #wq = WorkerPoolThreads(10)
+def makeListOfFilesToGet():
+    wq = WorkerPoolThreads(10)
 
-    con = sqlite.connect('podtrack.db')
-    
     rows=[r for r in con.cursor().execute("select * from podCast;")]
 
     for r in rows:
@@ -88,9 +92,13 @@ if __name__ == "__main__":
         processEntries(feed['entries'], r)
 
     wq.stop()
+    
+if __name__ == "__main__":
+    #wq = WorkerPoolSerial(10)
+    con = sqlite.connect('podtrack.db')
+    makeListOfFilesToGet()
 
-    #processEntries(feed['entries'], r[2])
-
+#for f in `ls` ; do mv $f `echo $f | sed 's/\?.*$//'` ; done
     f=open('get.sh', 'w')
     for l in filesToDl:
         f.write('wget ' + l + '\n')
